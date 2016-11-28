195242ef8f61717af6bdacf7a5c90caceac2992a
sandransigurdar <sandranordfjord11@gmail.com>
2016-11-28 17:22:48 +0000

    Unnid i Service.cpp, 2 sorting foll og main er breytt, en var useless adur og er thad enn

--- verklegt_vika_1/service.cpp
+++ verklegt_vika_1/service.cpp
@@ -1,6 +1,45 @@
 #include "service.h"
+#include <algorithm>
+
+using namespace std;
+
 
 Service::Service()
 {
- // Hlytur ad virka
+
 }
+// Erum her med allan listann i mismunandi butum. Nofnin, kynid, o.s.frv...
+
+void Service::sortByNameAscending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears)
+{
+    // Skoda betur hjemme
+    for(int i = 0; i < names.size(); i++)
+    {
+        //if(tolower(names[i])<tolower(names[i+1]))
+        //{
+
+        //}
+    }
+
+}
+
+//void Service::sortByNameDesending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+
+void Service::sortBySexF(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears)
+{
+    for(int i = 0; i < names.size(); i++)
+    {
+        if(sexes[i] == "f" || sexes[i] == "F")
+        {
+            string tmp = sexes[i];
+            sexes[i] = sexes[i + 1];
+            sexes[i+1] = tmp;
+        }
+    }
+}
+
+//void Service::sortbySexM(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+//void Service::sortByBirthAscending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+//void Service::sortByBirthDescending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+//void Service::sortByDeathAscending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+//void Service::sortByDeathDescending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
