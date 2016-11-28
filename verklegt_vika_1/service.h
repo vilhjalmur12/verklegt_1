e469188ddcffb8d5b7da8c99ec4d3589d5598289
Katli95 <ketill95@gmail.com>
2016-11-28 15:17:32 +0000

    Update nr. 1 - service

--- verklegt_vika_1/service.h
+++ verklegt_vika_1/service.h
@@ -11,15 +11,25 @@ class Service
 public:
     Service();
 
+    void addPerson(string name, string sex, int birthYear, int deathYear, vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+
+    void sort(string choice, vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+
+    void search(string query, vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+
+
+
+private:
+    /* ------------------Optional foll fyrir add person fallid--------------------------
+
     void addName(string name, vector<string>& names);
     void addSex(string sex, vector<string>& sexes);
     void addBirthYear(int birthYear, vector<int>& birthYears);
     void addDeathYear(int deathYear, vector<int>& deathYears, int birthYear);
 
-    void sort(string choice, vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+    */
 
 
-private:
     void sortByNameAscending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
     void sortByNameDesending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
     void sortBySexF(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
@@ -28,6 +38,8 @@ private:
     void sortByBirthDescending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
     void sortByDeathAscending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
     void sortByDeathDescending(vector<string>& names, vector<string>& sexes, vector<int>& birthYears, vector<int>& deathYears);
+
+
 
 };
 
