c9a7eecb676e4e20b83dba7ac99db5878c2dc794
ellensig <ellen16@gmail.com>
2016-11-29 16:35:31 +0000

    bæta við commentum í console.h

--- verklegt_vika_1/console.h
+++ verklegt_vika_1/console.h
@@ -13,12 +13,12 @@ public:
     void welcome(); //print out welcome note
     void viewOrInsert(); //print out choice menu for viewing or inserting
     char choice(); //make choice
-    void choiceMade(char choice_made);
+    void choiceMade(char choice_made); //choose view or insert
     void sorting_menu(); //print out the sorting menu
     string stringChoice(); //here you choose what kind of sorting you want to do
     void viewDisplay(); //prints out the list and sorting
     void toContinue(); //press enter to continue
-    void sorting(string str);
+    void sorting(string str); //here we print out the list how they want it to be sorted like
 
     //string str = stringChoice();
 
