40194c4d6cea58cb97ee1f3b652a10ef01ff7311
vilhjalmur12 <vilhjalmur12@ru.is>
2016-11-29 16:17:54 +0000

    uppfæra villuboða

--- verklegt_vika_1/console.h
+++ verklegt_vika_1/console.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Console
 {
-<<<<<<< HEAD
+
 public:
     Console();
     virtual ~Console();
@@ -23,37 +23,14 @@ public:
 
     //string str = stringChoice();
 
-    //void dataWriteError ();  // villi: error output fyrir database
+    void dataWriteError ();  // villi: error output fyrir database
+    void dataSearchNotFound ();
 
 
 protected:
 private:
-=======
-    public:
-        Console();
-        virtual ~Console();
-        void welcome(); //print out welcome note
-        void viewOrInsert(); //print out choice menu for viewing or inserting
-        char choice(); //make choice
-        void choiceMade(char choice_made);
-        void sorting_menu(); //print out the sorting menu
-        string stringChoice(); //here you choose what kind of sorting you want to do
-        void viewDisplay(); //prints out the list and sorting
 
 
-        void toContinue(); //press enter to continue
-    
-        void dataWriteError ();  // villi: error output fyrir database
-        void dataSearchNotFound ();
-        void run();
-
-
-    
-
-
-    protected:
-    private:
->>>>>>> 5e0ed330bddd229a3c616b4d3d6f8c208f8a4a68
 };
 
 #endif // CONSOLE_H
