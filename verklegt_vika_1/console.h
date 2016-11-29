349820dde31e44c6b51edc7c6823075ae524df07
Katli95 <ketill95@gmail.com>
2016-11-29 16:13:05 +0000

    háskráar fokk console

--- verklegt_vika_1/console.h
+++ verklegt_vika_1/console.h
@@ -1,5 +1,5 @@
-#ifndef CONSOLE_HPP
-#define CONSOLE_HPP
+#ifndef CONSOLE_H
+#define CONSOLE_H
 
 #include <string>
 
@@ -7,7 +7,6 @@ using namespace std;
 
 class Console
 {
-<<<<<<< HEAD
 public:
     Console();
     virtual ~Console();
@@ -28,32 +27,7 @@ public:
 
 protected:
 private:
-=======
-    public:
-        Console();
-        virtual ~Console();
-        void welcome(); //print out welcome note
-        void viewOrInsert(); //print out choice menu for viewing or inserting
-        char choice(); //make choice
-        void choiceMade(char choice_made);
-        void sorting_menu(); //print out the sorting menu
-        string stringChoice(); //here you choose what kind of sorting you want to do
-        void viewDisplay(); //prints out the list and sorting
 
-
-        void toContinue(); //press enter to continue
-    
-        void dataWriteError ();  // villi: error output fyrir database
-        void dataSearchNotFound ();
-        void run();
-
-
-    
-
-
-    protected:
-    private:
->>>>>>> 5e0ed330bddd229a3c616b4d3d6f8c208f8a4a68
 };
 
 #endif // CONSOLE_H
