3c526b19157342f93040932a5dcb3f0e241fcebd
helgah12 <helgah12@ru.is>
2016-12-01 15:47:47 +0000

    laga comment og stafsetningu

--- verklegt_vika_1/errorhandling.cpp
+++ verklegt_vika_1/errorhandling.cpp
@@ -14,12 +14,12 @@ ErrorHandling::~ErrorHandling (){ }
 
 void ErrorHandling::dataWriteError ()
 {
-    cout << "No database found!" << endl;
+    cout << "ERROR: No database found!" << endl;
 }
 
 void ErrorHandling::dataSearchNotFound ()
 {
-    cout << "No items found" << endl;
+    cout << "ERROR: No items found!" << endl;
 }
 
 void ErrorHandling::invalidName (int errorCode)
@@ -27,17 +27,17 @@ void ErrorHandling::invalidName (int errorCode)
     switch (errorCode)
     {
         case 1:
-            cout << "The name you entered is already exists in the list" << endl;
+            cout << "ERROR: The name you entered already exists in the list!" << endl;
             break;
         case 2:
-            cout << "Name can not include numbers" << endl;
+            cout << "ERROR: Name can not include numbers!" << endl;
             break;
     }
 }
 
 void ErrorHandling::invalidSex ()
 {
-    cout << "Sex must be either Male or Female" << endl;
+    cout << "ERROR: Gender has to be either male or female!" << endl;
 }
 
 void ErrorHandling::invalidYear (int errorCode)
@@ -45,22 +45,21 @@ void ErrorHandling::invalidYear (int errorCode)
     switch (errorCode)
     {
         case 1:
-            cout << "Year of death can not be before year of birth" << endl;
+            cout << "ERROR: Year of death can not be before year of birth!" << endl;
             break;
 
         case 2:
-            cout << "The Computer Scientst can not be born in the future" << endl;
+            cout << "ERROR: The Computer Scientist can not be born in the future!" << endl;
             break;
 
         case 3:
-            // Ath ad thessi dude faer samt ad halda afram i forritinu!!
-            cout << "Are you sure you want to add a Computer Scientist that was born before Christ? " << endl;
+            cout << "Are you sure you want to add a Computer Scientist that was born before Christ?" << endl;
             break;
         case 4:
-            cout << "Invalid characters in year." << endl;
+            cout << "ERROR: Invalid characters in year!" << endl;
             break;
         case 5:
-            cout << "Death cannot be registered for a future date" << endl;
+            cout << "ERROR: Death can not be registered for a future date!" << endl;
             break;
     }
 }
