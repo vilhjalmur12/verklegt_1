c994ae2e4af09f48df30cd8b9aea1639416dc727
Katli95 <ketill95@gmail.com>
2016-12-02 23:12:49 +0000

    villumeldingar á réttum stað

--- verklegt_vika_1/console.cpp
+++ verklegt_vika_1/console.cpp
@@ -602,22 +602,20 @@ void Console::readYears(int& YOB, int& YOD)
     bool validYears = false;
     do
     {
+        cout << scientistService.getErrorString();
         cout << throwError.catchError();
         cont = false;
         validYears = true;
 
         readBirthYear(YOB, cont);
-
         if(cont)
             continue;
 
         readDeathYear(YOD, cont);
-
         if(cont || YOD == maxDeathYear)
             continue;
 
         validYears = scientistService.validYears(YOB, YOD);
-
     }while(!validYears || cont);
 }
 
@@ -715,6 +713,7 @@ void Console::readNationality(string &nationality)
 {
     do
     {
+    cout << scientistService.getErrorString();
     cout << "Nationality: ";
     cin.ignore();
     getline(cin, nationality);
