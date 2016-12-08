c4e4e4937774fb6e70877f62da543e70ef9f88cd
Katli95 <ketill95@gmail.com>
2016-12-08 14:25:43 +0000

    Automatic false í deleted f. sci og cpu + foll taka ekki inn deleted

--- verklegt_vika_2/database.cpp
+++ verklegt_vika_2/database.cpp
@@ -75,7 +75,7 @@ vector<Scientist> database::pullScientists(string choice)
 
     vector<Scientist> scientists;
 
-    string command = "SELECT * FROM Scientists ORDER BY " + choice + ", last_name";
+    string command = "SELECT * FROM Scientists where deleted = 0 ORDER BY " + choice + ", last_name";
 
   //  QString Qchoice(choice.c_str());
     QString Qcommand(command.c_str());
@@ -100,6 +100,7 @@ vector<Computer> database::pullComputers(string choice)
     string command = "SELECT c.ID, name, year_of_build, type, built_or_not FROM Computers c "
                      "INNER JOIN cpuType t "
                      "ON t.ID = c.CPU_type_ID "
+                     "where deleted = 0 "
                      "ORDER BY " + choice +", name";
 
     //QString Qchoice(choice.c_str());
@@ -206,7 +207,7 @@ void database::initDatabase (const QString& username)
                        "(ID INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "
                         "First_name VARCHAR NOT NULL , Last_name VARCHAR NOT NULL , "
                         "Gender VARCHAR, Year_of_birth INTEGER, Year_of_death INTEGER, "
-                        "Nationality VARCHAR, Information VARCHAR)");
+                        "Nationality VARCHAR, Information VARCHAR, Deleted DEFAULT (0))");
 
 
         userQuery.exec ("CREATE  TABLE cpuType "
@@ -218,7 +219,8 @@ void database::initDatabase (const QString& username)
                         "Name VARCHAR NOT NULL, "
                         "Year_of_build INTEGER, "
                         "CPU_type_ID INTEGER, "
-                        "built_or_not BOOL, "
+                        "built_or_not BOOL,"
+                        "deleted DEFAULT (0), "
                         "FOREIGN KEY(CPU_type_ID) REFERENCES cpuType(ID))");
 
         userQuery.exec("CREATE TABLE scientist_computer_relations"
@@ -445,8 +447,10 @@ void database::searchComputersForSubstring(vector<Computer> &computers, const st
     query.prepare("SELECT c.ID, Name, year_of_build, type, built_or_not FROM computers c "
                   "INNER JOIN cpuType t "
                   "ON t.ID = c.CPU_type_ID "
-                  "WHERE c.name like :string "
-                  "OR t.type like :string "
+                  "WHERE (c.name like :string "
+                  "OR t.type like :string)"
+                  "AND deleted = 0"
+                  "ORDER BY name "
                   );
     query.bindValue(":string", searchQuery);
     query.exec();
@@ -460,9 +464,11 @@ void database::searchComputersForInt(vector<Computer> &computers, const int iQue
     query.prepare("Select c.ID, name, year_of_build, type, built_or_not FROM computers c "
                   "INNER JOIN cpuType t "
                   "ON t.ID = c.CPU_type_ID "
-                  "WHERE year_of_build = :int "
+                  "WHERE (year_of_build = :int "
                   "OR (year_of_build/100)*100 = :int "
-                  "OR (year_of_build/10)*10 = :int "
+                  "OR (year_of_build/10)*10 = :int) "
+                  "AND deleted = 0 "
+                  "ORDER BY name"
                 );
     query.bindValue(":int",iQuery);
     query.exec();
@@ -509,7 +515,8 @@ void database::addBuildersToComputers(vector<Computer> &computers)
         query.prepare("SELECT last_name FROM scientists s "
                       "LEFT OUTER JOIN scientist_computer_relations r "
                       "ON r.computerID = :ID "
-                      "WHERE ID = r.scientistID ");
+                      "WHERE ID = r.scientistID "
+                      "AND deleted = 0");
         query.bindValue(":ID", compID);
         query.exec();
 
@@ -528,11 +535,13 @@ void database::searchScientistsForSubstring(vector<Scientist> &scientists, const
 
     QSqlQuery query;
     query.prepare("SELECT * FROM scientists s "
-                  "WHERE s.first_name like :string "
+                  "WHERE (s.first_name like :string "
                   "OR s.last_name like :string "
                   "OR s.gender like :string "
                   "OR s.nationality like :string "
-                  "OR s.information like :string "
+                  "OR s.information like :string) "
+                  "AND deleted = 0 "
+                  "ORDER BY last_name "
                   );
     query.bindValue(":string", searchQuery);
     query.exec();
@@ -544,12 +553,13 @@ void database::searchScientistsForInt(vector<Scientist> &scientists, const int i
 {
     QSqlQuery query;
     query.prepare("Select * FROM scientists s "
-                  "WHERE year_of_birth = :int "
+                  "WHERE (year_of_birth = :int "
                   "OR (year_of_birth/100)*100 = :int "
                   "OR (year_of_birth/10)*10 = :int "
                   "OR  year_of_death = :int "
                   "OR (year_of_death/100)*100 = :int "
-                  "OR (year_of_death/10)*10 = :int "
+                  "OR (year_of_death/10)*10 = :int) "
+                  "AND deleted = 0 "
                 );
     query.bindValue(":int",iQuery);
     query.exec();
@@ -607,7 +617,8 @@ void database::adddBuiltComputersToScientists(vector<Scientist> &scientists)
         query.prepare("SELECT name FROM computers c "
                       "LEFT OUTER JOIN scientist_computer_relations r "
                       "ON r.ScientistID = :ID "
-                      "WHERE ID = r.computerID ");
+                      "WHERE ID = r.computerID "
+                      "AND deleted = 0 ");
         query.bindValue(":ID", sciID);
         query.exec();
 
@@ -622,7 +633,7 @@ void database::adddBuiltComputersToScientists(vector<Scientist> &scientists)
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-void database::selectData ()
+void database::selectData()
 {
 
     QSqlQuery query("SELECT * FROM cpuType");
