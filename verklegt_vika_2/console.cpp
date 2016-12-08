731556d4559e912edd0722ff500380ee324f5fb2
ellensig <ellen16@ru.is>
2016-12-08 17:30:10 +0000

    laga deleteComputer

--- verklegt_vika_2/console.cpp
+++ verklegt_vika_2/console.cpp
@@ -1483,7 +1483,7 @@ void Console::editOperation()
 void Console::deleteOperation()
 {
     vector<Scientist> scientists = scientistService.getScientists();
-    scientists = scientistService.getScientists();
+    vector<Computer> computers = scientistService.getComputers();
     string tmp = "n";
 
     do
@@ -1497,17 +1497,17 @@ void Console::deleteOperation()
             printScientists(scientists);
 
             int ID;
-            cout << "ID -> ";
+            cout << "Nr. -> ";
             cin >> ID;
             scientistService.deleteScientist(ID);
             tmp = "n";
         }
         else if (choice_made == "c")
         {
-            printScientists(scientists);
+            printComputers(computers);
 
             int ID;
-            cout << "ID -> ";
+            cout << "Nr. -> ";
             cin >> ID;
             scientistService.deleteComputer(ID);
             tmp = "n";
