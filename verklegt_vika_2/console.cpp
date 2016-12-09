b583c38b2e6ec1dfc9affc6179f74c831ff68e67
ellensig <ellen16@ru.is>
2016-12-09 23:18:19 +0000

    laga til

--- verklegt_vika_2/console.cpp
+++ verklegt_vika_2/console.cpp
@@ -946,7 +946,7 @@ Computer Console::makeNewComputer()
                       askToRelate
     Spyr notandan hvort hann vilji tengja tölvuna/vísindamanninn við
     vísindamann/tölvu og gerir villutékk
-    @return(string option) - tekur inn hvaða streng á að prenta út
+    @return(string option) - tekur inn strenginn sem á að prenta út
  ******************************************************************/
 
 string Console::askToRelate(string option)
@@ -1163,6 +1163,11 @@ void Console::addRelationsToSci(int sIndex)
     }while(choice == "y");
 }
 
+/******************************************************************
+                      recycleScientist
+         Setja inn vísindamann sem búið var að eyða
+ ******************************************************************/
+
 void Console::recycledBin()
 {
     string cont;
@@ -1193,6 +1198,11 @@ void Console::recycledBin()
     }while(cont == "y");
 }
 
+/******************************************************************
+                      recycleScientists
+         Setja inn vísindamann sem búið var að eyða
+ ******************************************************************/
+
 void Console::recycleScientists()
 {
     unsigned int index;
@@ -1216,6 +1226,11 @@ void Console::recycleScientists()
         scientistService.restoreScientist(index);
     }
 }
+
+/******************************************************************
+                      recycleComputers
+         Setja inn tölvu sem búið var að eyða
+ ******************************************************************/
 
 void Console::recycleComputers()
 {
