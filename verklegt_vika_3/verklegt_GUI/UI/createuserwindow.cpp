ab17646ed616df2869d5e87f4ba51395bfedba72
asgeiro14 <asgeiro14@ru.is>
2016-12-16 10:59:07 +0000

    comment

--- verklegt_vika_3/verklegt_GUI/UI/createuserwindow.cpp
+++ verklegt_vika_3/verklegt_GUI/UI/createuserwindow.cpp
@@ -1,6 +1,10 @@
 #include "createuserwindow.h"
 #include "ui_createuserwindow.h"
 
+/******************************************************************
+                      Constructor / Destructor
+*******************************************************************/
+
 createUserWindow::createUserWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::createUserWindow)
@@ -12,6 +16,12 @@ createUserWindow::~createUserWindow()
 {
     delete ui;
 }
+
+/******************************************************************
+                      on_pushButton_clicked
+ Tekur við upplýsingum fyrir nýjan notanada. Notandanaf, lykilorð,
+ staðfestingu á lykilorði, nafn og eftirnaf.
+*******************************************************************/
 
 void createUserWindow::on_pushButton_clicked()
 {
@@ -46,6 +56,11 @@ void createUserWindow::on_pushButton_clicked()
     }
 
 }
+
+/******************************************************************
+                      comparePassword
+     Ber saman lykilorðið og staðfestinguna á því frá notanada.
+*******************************************************************/
 
 bool createUserWindow::comparePassword()
 {
